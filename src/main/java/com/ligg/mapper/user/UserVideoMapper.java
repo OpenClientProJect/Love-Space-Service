9d22a7335a6ca0450d284cc24a785d86b5b7faf3
LiggPlus <wenzhouli06@gmail.com>
2025-04-11 15:54:46 +0800

    用户视频默认上传草稿表

--- src/main/java/com/ligg/mapper/user/UserVideoMapper.java
+++ src/main/java/com/ligg/mapper/user/UserVideoMapper.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Mapper
 public interface UserVideoMapper {
     // 添加用户和视频
-    @Insert("insert into video(title,cover,user_id,content,video_url,create_time,update_time)" +
+    @Insert("insert into video_draft(title,cover,user_id,content,video_url,create_time,update_time)" +
             "values(#{title},#{cover},#{userId},#{content},#{videoUrl},NOW(),NOW())")
     void add(Video video);
 
