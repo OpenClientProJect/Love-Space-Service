b09791e95699b5d502f772999b573460b3633a8d
LiggPlus <wenzhouli06@gmail.com>
2025-05-08 16:38:09 +0800

    update 2.0

---
+++ src/main/java/com/love/service/impl/VideoServiceImpl.java
@@ -0,0 +1,41 @@
+package com.love.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.love.mapper.VideoMapper;
+import com.love.pojo.Video;
+import com.love.service.VideoService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class VideoServiceImpl implements VideoService {
+
+    @Autowired
+    VideoMapper videoMapper;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    // 查询所有视频
+    @Override
+    public List<Video> list(Integer categoryId) {
+        return videoMapper.list(categoryId);
+    }
+
+    // 根据id查询视频
+    @Override
+    public Video findById(Integer id) {
+        return videoMapper.findById(id);
+    }
+
+    @Override
+    public List<Video> getCollectList(Long userId) {
+        return videoMapper.findVideoFavorite(userId);
+    }
+}
